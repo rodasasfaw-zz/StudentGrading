0df6d15c13050cbbab3cc8bac6239f4f90eae182
rodasasfaw <rodasasfaw4@gmail.com>
2018-01-25 13:57:05 -0500

    this is my commit message for grading project

--- src/main/java/com/example/demospringboot/MainController.java
+++ src/main/java/com/example/demospringboot/MainController.java
@@ -1,12 +1,7 @@
 package com.example.demospringboot;
-
-
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Scanner;
-
 
 @RestController
 public class MainController {
@@ -14,14 +9,31 @@ public class MainController {
 
     public String showIndex()
     {
+
+        char letter_grade;
+        int test_score_percentage;
+        int test_score;
+
         System.out.println("Enter a value and then go to localhost:8080");
-
-        String userName="";
         Scanner keyboard = new Scanner(System.in);
-        userName = keyboard.nextLine();
-        System.out.println("Hey john" +" "+userName);
-        return"Hey john"+ " "+userName;
+        test_score = keyboard.nextInt();
+        test_score_percentage =(test_score/50)*100;
 
+        if(test_score_percentage>=90&&test_score_percentage<=100)
+         letter_grade = 'A';
 
+         else if(test_score_percentage>=80&&test_score_percentage<=89)
+            letter_grade = 'B';
+
+           else if(test_score_percentage>=70&&test_score_percentage<=79)
+              letter_grade = 'C';
+
+              else if(test_score_percentage>=60&&test_score_percentage<=69)
+               letter_grade = 'D';
+                 else
+                letter_grade = 'F';
+
+System.out.println("student's grade is:" + letter_grade);
+return "student's grade is:" + letter_grade;
     }
 }
